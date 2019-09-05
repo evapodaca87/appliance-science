23e297b09e2ca244a639180c55c0f11a907daa2a
Evan Apodaca <evanapodaca@Admins-MBP-7.domain>
2019-09-04 20:37:02 -0600

    added skeleton info

--- src/App.js
+++ src/App.js
@@ -6,17 +6,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="logo" alt="logo" />
         <h1>Appliance Science</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body>
+        <p>Our goal is to treat every customer as if our world revolves around them.</p>
+        <p>PLACEHOLDER DESCRIPTION</p>
+        <p>Serving Denver, CO and the surrounding metro area</p>
+        <p>720-443-7233</p>
+        <p>applisci@outlook.com</p>
+      </body>
     </div>
   );
 }
